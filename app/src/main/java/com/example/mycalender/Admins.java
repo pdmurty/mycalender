edffc62b0893bca163acb3a7e61ac6353d2fd1d3
pdmurty <pdmurty@hotmail.com>
2020-10-12 19:24:55 +0530

    Create Admins.java

---
+++ app/src/main/java/com/example/mycalender/Admins.java
@@ -0,0 +1,24 @@
+package com.example.mycalender;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "admincodes")
+public class Admins {
+    @NonNull
+    @ColumnInfo(name= "admin1code")
+    String admin1code;
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name ="id")
+    int id;
+    @NonNull
+    @ColumnInfo(name = "country")
+    String country;
+    @NonNull
+    @ColumnInfo(name = "asciiname")
+    String adminname;
+
+}
