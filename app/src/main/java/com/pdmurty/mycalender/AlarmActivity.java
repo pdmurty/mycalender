dc9437b709dbb3aea559ee35d6fceec4a784fa44
pdmurty <pdmurty@hotmail.com>
2021-04-15 12:41:10 +0530

    Create AlarmActivity.java

---
+++ app/src/main/java/com/pdmurty/mycalender/AlarmActivity.java
@@ -0,0 +1,14 @@
+package com.pdmurty.mycalender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
