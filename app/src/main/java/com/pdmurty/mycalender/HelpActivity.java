6fe6087dabf95a7991ab7ae024c59f845e357f35
pdmurty <pdmurty@hotmail.com>
2021-04-15 12:25:49 +0530

    Create HelpActivity.java

---
+++ app/src/main/java/com/pdmurty/mycalender/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.pdmurty.mycalender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
+}
