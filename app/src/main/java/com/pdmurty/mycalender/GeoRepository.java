5246862f700b9259cd7c69f2eb95651535c4899b
pdmurty <pdmurty@hotmail.com>
2021-04-15 12:30:18 +0530

    Create GeoRepository.java

---
+++ app/src/main/java/com/pdmurty/mycalender/GeoRepository.java
@@ -0,0 +1,34 @@
+package com.pdmurty.mycalender;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class GeoRepository {
+    private static GeoDao mgeoDao;
+    private static GeoRoomDb db;
+    private LiveData<List<Geonames>> mAllNames;
+    GeoRepository(Context context){
+         db = GeoRoomDb.getDatabase(context);
+         mgeoDao = db.getDao();
+         mAllNames = mgeoDao.getAllNames();
+        }
+
+     public LiveData<List<Geonames>>getAllNames(){ return mAllNames;}
+
+     public LiveData<List<Countries>>getAllCountries(){
+        return mgeoDao.getCountries();}
+
+        public LiveData<List<Geonames>> getAllnames(String country_code,String admin, String like) {
+            return mgeoDao.getAllNames(country_code, admin, like);
+        }
+
+        public LiveData<List<Admins>> getAllAdmins(String country_code) {
+            return mgeoDao.getAllAdmins(country_code);
+        }
+    public LiveData< List<GeoDao.GeoLoc>> getgeolocs (String country , String admin, String like){
+        return mgeoDao.getgeolocs (country , admin, like);
+    }
+}
